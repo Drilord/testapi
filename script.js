7a13be70bd759229186b0aa864857eef263ea419
Drilord <133545475+Drilord@users.noreply.github.com>
2023-10-10 22:40:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -25,19 +25,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('fetchFirst4').addEventListener('click', () => {
         const apiUrl =
-            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2022-01-01 TO 2022-04-31]&page_length=10000&page=1';
+            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2021-01-01 TO 2021-04-31]&page_length=10000&page=1';
         fetchData(apiUrl);
     });
 
     document.getElementById('fetchNext4').addEventListener('click', () => {
         const apiUrl =
-            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2022-05-01 TO 2022-08-31]&page_length=10000&page=1';
+            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2021-05-01 TO 2021-08-31]&page_length=10000&page=1';
         fetchData(apiUrl);
     });
 
     document.getElementById('fetchLast4').addEventListener('click', () => {
         const apiUrl =
-            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2022-09-01 TO 2022-12-31]&page_length=10000&page=1';
+            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2021-09-01 TO 2021-12-31]&page_length=10000&page=1';
         fetchData(apiUrl);
     });
 
