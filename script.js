b816c6bc5b3858bfbb79eeb2dd0e54408bff81c8
Drilord <133545475+Drilord@users.noreply.github.com>
2023-10-10 22:27:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
     const apiKey = 'GU9Zx0b21jFQaVWBjRk-d2InTTB0Q1dUSkROa2hQTkZKNWVtSmpjbWh1WlRaalZVWksn'; // Replace with your JWPlayer API key
     const resultDiv = document.getElementById('result');
-    
+
     function fetchData(apiUrl) {
         fetch(apiUrl, {
             headers: {
-                Authorization: `Bearer ${apiKey}`
-            }
+                Authorization: `Bearer ${apiKey}`,
+            },
         })
-            .then(response => {
+            .then((response) => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 }
                 return response.json();
             })
-            .then(data => {
+            .then((data) => {
                 const productionTypes = countProductionTypes(data.media);
                 resultDiv.innerHTML = displayProductionTypes(productionTypes);
             })
-            .catch(error => {
+            .catch((error) => {
                 resultDiv.innerHTML = `API Request Error: ${error}`;
             });
     }
 
     document.getElementById('fetchFirst4').addEventListener('click', () => {
-        const apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-01-01 TO 2023-04-31]&page_length=3000&page=1';
+        const apiUrl =
+            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-01-01 TO 2023-04-31]&page_length=3000&page=1';
         fetchData(apiUrl);
     });
 
     document.getElementById('fetchNext4').addEventListener('click', () => {
-        const apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-05-01 TO 2023-08-31]&page_length=3000&page=1';
+        const apiUrl =
+            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-05-01 TO 2023-08-31]&page_length=3000&page=1';
         fetchData(apiUrl);
     });
 
     document.getElementById('fetchLast4').addEventListener('click', () => {
-        const apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-09-01 TO 2023-12-31]&page_length=3000&page=1';
+        const apiUrl =
+            'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-09-01 TO 2023-12-31]&page_length=3000&page=1';
         fetchData(apiUrl);
     });
 
     function countProductionTypes(data) {
         const productionTypeCounts = {};
-        data.forEach(item => {
-            const productionType = item.productionType;
-            if (productionTypeCounts.hasOwnProperty(productionType)) {
-                productionTypeCounts[productionType]++;
-            } else {
-                productionTypeCounts[productionType] = 1;
+        data.forEach((item) => {
+            const customParams = item.metadata.custom_params;
+            if (customParams && customParams.productionType) {
+                const productionType = customParams.productionType;
+                if (productionTypeCounts.hasOwnProperty(productionType)) {
+                    productionTypeCounts[productionType]++;
+                } else {
+                    productionTypeCounts[productionType] = 1;
+                }
             }
         });
         return productionTypeCounts;
