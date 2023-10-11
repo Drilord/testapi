b06927fb60bc169ff8a6b771799813444f4df829
Drilord <133545475+Drilord@users.noreply.github.com>
2023-10-10 20:55:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const apiKey = 'GU9Zx0b21jFQaVWBjRk-d2InTTB0Q1dUSkROa2hQTkZKNWVtSmpjbWh1WlRaalZVWksn'; // Replace with your JWPlayer API key
-    let apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2022-01-01 TO 2023-04-31]&page_length=7000&page=1';
-    
-    const fetchDataButtonFirst4 = document.getElementById('fetchFirst4');
-    const fetchDataButtonNext4 = document.getElementById('fetchNext4');
-    const fetchDataButtonLast4 = document.getElementById('fetchLast4');
     const resultDiv = document.getElementById('result');
     
-    fetchDataButtonFirst4.addEventListener('click', () => {
-        fetchAndDisplayData(apiKey, apiUrl);
-    });
-    
-    fetchDataButtonNext4.addEventListener('click', () => {
-        apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-05-01 TO 2023-08-31]&page_length=7000&page=1';
-        fetchAndDisplayData(apiKey, apiUrl);
-    });
-    
-    fetchDataButtonLast4.addEventListener('click', () => {
-        apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-09-01 TO 2023-12-31]&page_length=7000&page=1';
-        fetchAndDisplayData(apiKey, apiUrl);
-    });
-
-    function fetchAndDisplayData(apiKey, apiUrl) {
+    function fetchData(apiUrl) {
         fetch(apiUrl, {
             headers: {
                 Authorization: `Bearer ${apiKey}`
@@ -34,32 +15,46 @@ document.addEventListener('DOMContentLoaded', () => {
                 return response.json();
             })
             .then(data => {
-                // Count the production types and display
-                const mediaTypes = countMediaTypes(data);
-                resultDiv.innerHTML = displayMediaTypes(mediaTypes);
+                const productionTypes = countProductionTypes(data.media);
+                resultDiv.innerHTML = displayProductionTypes(productionTypes);
             })
             .catch(error => {
                 resultDiv.innerHTML = `API Request Error: ${error}`;
             });
     }
 
-    function countMediaTypes(data) {
-        const mediaTypeCounts = {};
-        data.media.forEach(item => {
-            const mediaType = item.media_type;
-            if (mediaTypeCounts.hasOwnProperty(mediaType)) {
-                mediaTypeCounts[mediaType]++;
+    document.getElementById('fetchFirst4').addEventListener('click', () => {
+        const apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-01-01 TO 2023-04-31]&page_length=4&page=1';
+        fetchData(apiUrl);
+    });
+
+    document.getElementById('fetchNext4').addEventListener('click', () => {
+        const apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-05-01 TO 2023-08-31]&page_length=4&page=1';
+        fetchData(apiUrl);
+    });
+
+    document.getElementById('fetchLast4').addEventListener('click', () => {
+        const apiUrl = 'https://api.jwplayer.com/v2/sites/MHI47Cs9/media/?q=created:[2023-09-01 TO 2023-12-31]&page_length=4&page=1';
+        fetchData(apiUrl);
+    });
+
+    function countProductionTypes(data) {
+        const productionTypeCounts = {};
+        data.forEach(item => {
+            const productionType = item.productionType;
+            if (productionTypeCounts.hasOwnProperty(productionType)) {
+                productionTypeCounts[productionType]++;
             } else {
-                mediaTypeCounts[mediaType] = 1;
+                productionTypeCounts[productionType] = 1;
             }
         });
-        return mediaTypeCounts;
+        return productionTypeCounts;
     }
 
-    function displayMediaTypes(mediaTypes) {
-        let output = 'Media Types and Counts:<br>';
-        for (const type in mediaTypes) {
-            output += `Type: ${type} Count: ${mediaTypes[type]}<br>`;
+    function displayProductionTypes(productionTypes) {
+        let output = 'Production Types and Counts:<br>';
+        for (const type in productionTypes) {
+            output += `type: ${type} count: ${productionTypes[type]}<br>`;
         }
         return output;
     }
