52aac5870b0e6763f25875b7354e3e202197db78
Drilordubu <diego.gonzalezsilva@gmail.com>
2025-01-03 15:41:10 -0600

    sfsdf

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-const exp = require('express');
+const express = require('express');
 const jsf = require('jsonfile')
+const exp = express();
+const cors = require('cors');
+
+exp.use(cors()); /*modificar despues para solo  dar acceso al container de nginx
+codigo de ejemplo:
+app.use(cors({
+  origin: 'http://localhost', // permite requests desde este origin
+  methods: 'GET, POST, PUT, DELETE', // especifica los methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // y los headers
+}));
+
+*/ 
 
 //ruteo
-exp.get('/bombSol', (req, res)=>{
-    readjson(datos.json)
-    res.send
+exp.get('/bombSol', async (req, res)=>{
+    console.log(req.url);
+    try {
+        const data = await readjson('datos.json');
+        console.log(data); // despues de leer
+        res.send(JSON.stringify(data)); 
+      } catch (error) {
+        console.error('Error leyendo el archivo json:', error);
+        res.status(500).send('Error al leer los datos');
+      }
+  
 
 });
+exp.get('/cots', async (req, res)=>{
+    console.log(req.url);
+    try {
+        const data = await readjson('cots.json')
+        console.log(data); // despues de leer
+        res.send(JSON.stringify(data)); 
+      } catch (error) {
+        console.error('Error leyendo el archivo json:', error);
+        res.status(500).send('Error al leer los datos');
+      }
+});
+
  const PORT = process.env.PORT|| 3000;
+ 
 
  async function readjson(filePath) {
  try {
-     const data = await jsonfile.readFile(filePath);
-     if(data){return data;}
+     const data = await jsf.readFile(filePath);
+         if(data){ return data;}
      else{console.log('Error de lectura'); return;}
  
    } catch (err) {
-     console.error('Error reading JSON:', err);
+     console.error('Error leyendo JSON:', err);
    }
 }
+
+exp.listen(PORT, ()=>{
+    console.log('Servidor escuchando en: ', PORT)
+});
